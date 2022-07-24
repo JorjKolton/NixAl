c42efc1e9a8c6dbc36143e15346a5f366d1b2805
Sergey Sobchenko <ssobchenko@gmail.com>
2022-07-25 02:12:24 +0300

    Add thenComparing

--- src/main/java/com/nixal/ssobchenko/Main.java
+++ src/main/java/com/nixal/ssobchenko/Main.java
@@ -14,36 +14,10 @@ public class Main {
     private static final BusService BUS_SERVICE = new BusService(new BusesRepository());
 
     public static void main(String[] args) throws InterruptedException {
-        Bus one = BUS_SERVICE.createAndSave(1).get(0);
-        Bus two = BUS_SERVICE.createAndSave(1).get(0);
-        Bus three = BUS_SERVICE.createAndSave(1).get(0);
-        Bus four = BUS_SERVICE.createAndSave(1).get(0);
-        Bus five = BUS_SERVICE.createAndSave(1).get(0);
-
-        Garage<Vehicle> garage = new Garage<>();
-        garage.add(one, 1);
-        garage.add(two, 2);
-        garage.add(three, 3);
-        garage.add(four, 4);
-
-        Iterator<Vehicle> vi = garage.iterator();
-        while (vi.hasNext()) {
-            System.out.println(vi.next());
-        }
-
-        System.out.println("--------------");
-
-        garage.showLastRestylingData();
-
-        Thread.sleep(5000);
-        garage.add(five, 5);
-
-        garage.showLastRestylingData();
-
-        System.out.println("----------------");
-
         List<Bus> buses = new ArrayList<>(BUS_SERVICE.createAndSave(10));
-        buses.sort(new Bus.PriceComparator().reversed());
+        buses.sort(new Bus.PriceComparator().reversed()
+                .thenComparing(new Vehicle.ModelComparator()
+                        .thenComparing(new Vehicle.IdComparator())));
 
         for (Bus b : buses) {
             System.out.println(b.getPrice());
