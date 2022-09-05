82be66f9e23ee9b9cb094bc2e7cbf535d66594cf
Sergey Sobchenko <ssobchenko@gmail.com>
2022-09-06 01:07:08 +0300

    yep

--- src/main/java/com/nixal/ssobchenko/repository/mongo/MongoInvoicesRepository.java
+++ src/main/java/com/nixal/ssobchenko/repository/mongo/MongoInvoicesRepository.java
@@ -8,7 +8,11 @@ import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Sorts;
 import com.nixal.ssobchenko.config.GsonUtil;
 import com.nixal.ssobchenko.config.MongoUtil;
+import com.nixal.ssobchenko.model.vehicle.Bus;
+import com.nixal.ssobchenko.model.vehicle.Car;
 import com.nixal.ssobchenko.model.vehicle.Invoice;
+import com.nixal.ssobchenko.model.vehicle.Motorcycle;
+import com.nixal.ssobchenko.model.vehicle.Vehicle;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
@@ -18,6 +22,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 import static com.mongodb.client.model.Aggregates.group;
@@ -46,9 +51,9 @@ public class MongoInvoicesRepository {
             throw new IllegalArgumentException("Invoice must be not null");
         }
         Document parse = mapToDoc(invoice);
-        final List<String> vehicles = invoice.getVehicles()
+        final List<Document> vehicles = invoice.getVehicles()
                 .stream()
-                .map(v -> v.getId().toLowerCase())
+                .map(v -> Document.parse(gson.toJson(v)))
                 .toList();
         parse.append("Vehicles", vehicles);
         invoices.insertOne(parse);
@@ -56,9 +61,27 @@ public class MongoInvoicesRepository {
     }
 
     public Optional<Invoice> findById(String id) {
-        return Optional.of(invoices.find(new Document("_id", id))
-                .map(motorcycle -> gson.fromJson(motorcycle.toJson(), Invoice.class))
-                .into(new ArrayList<>()).get(0));
+        @SuppressWarnings("unchecked")
+        List<Document> documents = (List<Document>) Objects
+                .requireNonNull(invoices.find(new Document("_id", id)).first())
+                .get("Vehicles");
+        List<Vehicle> vehicles = new ArrayList<>();
+        for (Document document : documents) {
+            if (document.containsValue("CAR")) {
+                vehicles.add(gson.fromJson(document.toJson(), Car.class));
+            }
+            if (document.containsValue("BUS")) {
+                vehicles.add(gson.fromJson(document.toJson(), Bus.class));
+            }
+            if (document.containsValue("MOTORCYCLE")) {
+                vehicles.add(gson.fromJson(document.toJson(), Motorcycle.class));
+            }
+        }
+        Invoice found = invoices.find(new Document("_id", id))
+                .map(invoice -> gson.fromJson(invoice.toJson(), Invoice.class))
+                .into(new ArrayList<>()).get(0);
+        found.setVehicles(vehicles);
+        return Optional.of(found);
     }
 
     public int getCountOfInvoices() {
@@ -67,14 +90,14 @@ public class MongoInvoicesRepository {
 
     public List<Invoice> getAll() {
         return invoices.find()
-                .map(invoice -> gson.fromJson(invoice.toJson(), Invoice.class))
+                .map(invoice -> findById(invoice.getString("_id")).get())
                 .into(new ArrayList<>());
     }
 
     public List<Invoice> getAllInvoicesWherePriceBiggerThan(int price) {
         Bson filter = Filters.gt("price", price);
         return invoices.find(filter)
-                .map(item -> gson.fromJson(item.toJson(), Invoice.class))
+                .map(invoice -> findById(invoice.getString("_id")).get())
                 .into(new ArrayList<>());
     }
 
