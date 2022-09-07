e46bbea52dd1103b25a83b3c65c1fa4a5e20705e
Sergey Sobchenko <ssobchenko@gmail.com>
2022-09-08 00:49:17 +0300

    heroku third try

--- src/main/java/com/nixal/ssobchenko/config/JDBCConfig.java
+++ src/main/java/com/nixal/ssobchenko/config/JDBCConfig.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class JDBCConfig {
-    private static final String URL = "postgres://zsvuuseouarbfy:a10777c96b9ac8426c04684acc0c392003d978a2ee99f2f2b54ab0ba5ea9d1fb@ec2-54-204-241-136.compute-1.amazonaws.com:5432/d1miis3dfaq9pt";
+    private static final String URL = "jdbc:postgresql://ec2-54-204-241-136.compute-1.amazonaws.com:5432/d1miis3dfaq9pt";
     private static final String USER = "zsvuuseouarbfy";
     private static final String PASSWORD = "a10777c96b9ac8426c04684acc0c392003d978a2ee99f2f2b54ab0ba5ea9d1fb";
 
