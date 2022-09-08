c952a93644504221d73940c819050259526375bd
Sergey Sobchenko <ssobchenko@gmail.com>
2022-09-08 16:36:10 +0300

    flyway

--- src/main/java/com/nixal/ssobchenko/Main.java
+++ src/main/java/com/nixal/ssobchenko/Main.java
@@ -1,15 +1,10 @@
 package com.nixal.ssobchenko;
 
-import com.nixal.ssobchenko.config.SQLScript;
-import com.nixal.ssobchenko.model.vehicle.Invoice;
-import com.nixal.ssobchenko.model.vehicle.Vehicle;
 import com.nixal.ssobchenko.service.BusService;
 import com.nixal.ssobchenko.service.CarService;
 import com.nixal.ssobchenko.service.InvoiceService;
 import com.nixal.ssobchenko.service.MotorcycleService;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.flywaydb.core.Flyway;
 
 public class Main {
     private static final CarService CAR_SERVICE = CarService.getInstance();
@@ -18,36 +13,43 @@ public class Main {
     private static final InvoiceService INVOICE_SERVICE = InvoiceService.getInstance();
 
     public static void main(String[] args) {
-        SQLScript.createAllTables();
 
-        List<Vehicle> vehicles1 = new ArrayList<>();
-        vehicles1.addAll(CAR_SERVICE.createAndSave(2));
-        vehicles1.addAll(BUS_SERVICE.createAndSave(2));
-        vehicles1.addAll(MOTORCYCLE_SERVICE.createAndSave(2));
-
-        List<Vehicle> vehicles2 = new ArrayList<>();
-        vehicles2.addAll(CAR_SERVICE.createAndSave(2));
-        vehicles2.addAll(BUS_SERVICE.createAndSave(2));
-        vehicles2.addAll(MOTORCYCLE_SERVICE.createAndSave(2));
-
-        List<Vehicle> vehicles3 = new ArrayList<>();
-        vehicles3.addAll(CAR_SERVICE.createAndSave(2));
-        vehicles3.addAll(BUS_SERVICE.createAndSave(2));
-        vehicles3.addAll(MOTORCYCLE_SERVICE.createAndSave(2));
-
-        INVOICE_SERVICE.createAndSaveInvoice(vehicles1);
-        INVOICE_SERVICE.createAndSaveInvoice(vehicles2);
-        INVOICE_SERVICE.createAndSaveInvoice(vehicles3);
-
-        System.out.println("Count of invoices = " + INVOICE_SERVICE.countOfInvoices());
-        System.out.println("Count of invoices where price bigger than 20000 = " +
-                INVOICE_SERVICE.getInvoicesWherePriceBiggerThan(20000).size());
-        System.out.println(INVOICE_SERVICE.groupInvoicesBySum());
-
-        INVOICE_SERVICE.getAll()
-                .stream()
-                .map(Invoice::getVehicles)
-                .forEach(System.out::println);
-
+        Flyway flyway = Flyway.configure()
+                .dataSource("jdbc:postgresql://localhost:5432/Vehicles", "postgres", "postgres")
+                .baselineOnMigrate(true)
+                .locations("db.migration")
+                .load();
+        flyway.migrate();
+//        SQLScript.createAllTables();
+//
+//        List<Vehicle> vehicles1 = new ArrayList<>();
+//        vehicles1.addAll(CAR_SERVICE.createAndSave(2));
+//        vehicles1.addAll(BUS_SERVICE.createAndSave(2));
+//        vehicles1.addAll(MOTORCYCLE_SERVICE.createAndSave(2));
+//
+//        List<Vehicle> vehicles2 = new ArrayList<>();
+//        vehicles2.addAll(CAR_SERVICE.createAndSave(2));
+//        vehicles2.addAll(BUS_SERVICE.createAndSave(2));
+//        vehicles2.addAll(MOTORCYCLE_SERVICE.createAndSave(2));
+//
+//        List<Vehicle> vehicles3 = new ArrayList<>();
+//        vehicles3.addAll(CAR_SERVICE.createAndSave(2));
+//        vehicles3.addAll(BUS_SERVICE.createAndSave(2));
+//        vehicles3.addAll(MOTORCYCLE_SERVICE.createAndSave(2));
+//
+//        INVOICE_SERVICE.createAndSaveInvoice(vehicles1);
+//        INVOICE_SERVICE.createAndSaveInvoice(vehicles2);
+//        INVOICE_SERVICE.createAndSaveInvoice(vehicles3);
+//
+//        System.out.println("Count of invoices = " + INVOICE_SERVICE.countOfInvoices());
+//        System.out.println("Count of invoices where price bigger than 20000 = " +
+//                INVOICE_SERVICE.getInvoicesWherePriceBiggerThan(20000).size());
+//        System.out.println(INVOICE_SERVICE.groupInvoicesBySum());
+//
+//        INVOICE_SERVICE.getAll()
+//                .stream()
+//                .map(Invoice::getVehicles)
+//                .forEach(System.out::println);
+//
     }
 }
---
+++ src/main/resources/db.migration/V2_0__insert_vehicles.sql
@@ -0,0 +1,65 @@
+insert into cars (id, vehicle_type, model, body_type, manufacturer, price, created)
+values (
+'testID1',
+'car',
+800,
+'CABRIOLET',
+'AUDI',
+7800,
+'2022-09-07 17:29:34.779129'
+);
+
+insert into cars (id, vehicle_type, model, body_type, manufacturer, price, created)
+values (
+'testID2',
+'car',
+801,
+'COUPE',
+'VOLKSWAGEN',
+9400,
+'2022-09-07 17:29:34.779129'
+);
+
+insert into buses (id, vehicle_type, model, manufacturer, price, seats, created)
+values (
+'testID3',
+'bus',
+105,
+'SCANIA',
+17600,
+52,
+'2022-09-07 17:29:34.779129'
+);
+
+insert into buses (id, vehicle_type, model, manufacturer, price, seats, created)
+values (
+'testID4',
+'bus',
+247,
+'TATA',
+23000,
+18,
+'2022-09-07 17:29:34.779129'
+);
+
+insert into motorcycles (id, vehicle_type, model, body_type, manufacturer, price, created)
+values (
+'testID5',
+'motorcycle',
+100,
+'CRUISER',
+'KTM',
+1700,
+'2022-09-07 17:29:34.779129'
+);
+
+insert into motorcycles (id, vehicle_type, model, body_type, manufacturer, price, created)
+values (
+'testID6',
+'motorcycle',
+297,
+'MOTOCROSS',
+'KAWASAKI',
+2500,
+'2022-09-07 17:29:34.779129'
+);
